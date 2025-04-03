fc7e37e758e529a9b279d39502e5abaf7a6f8670
Itikatchi <yann.neuville@outlook.fr>
2025-04-03 09:37:03 +0200

    Espece de bouffons

--- Code/src/main/java/fsiAdministration/DAO/EtudiantDAO.java
+++ Code/src/main/java/fsiAdministration/DAO/EtudiantDAO.java
@@ -82,7 +82,7 @@ public class EtudiantDAO extends DAO<Etudiant>{
 
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/FSI_GestionAdmin","postgres","cannelle01");
+            Connection connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/FSI_GestionAdmin","postgres","EfaZynWu");
 //            Connection  connect = DriverManager.getConnection("jdbc:postgresql://localhost:5433/FSI_GestionAdmin","postgres","postgreSQL");
 
             String sql = "SELECT * FROM etudiant";
